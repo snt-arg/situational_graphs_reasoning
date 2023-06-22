6ebf50a1017b7938a49b4f410aa134bd39ca6898
JoseAndresMR <josmilrom@gmail.com>
2023-06-22 16:58:00 +0200

    [feat] - added noiese an views in CustomSquaredRoomDataset

--- graph_reasoning/main.py
+++ graph_reasoning/main.py
@@ -1,4 +1,4 @@
-from CustomSquaredRoomDataset import CustomSquaredRoomNetworkxGraphs
+from graph_reasoning.SquaredRoomDatasetGenerator import SquaredRoomDatasetGenerator
 from GNNWrapper import GNNWrapper
 import matplotlib.pyplot as plt
 import json, os
@@ -15,24 +15,24 @@ import json, os
 with open(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),"config","config.json")) as f:
     settings = json.load(f)
 
-dataset_generator = CustomSquaredRoomNetworkxGraphs(settings)
-# room_clustering_dataset = dataset.get_ws2room_clustering_datalodaer()
-filtered_nxdataset = dataset_generator.get_filtered_datset(["ws"],["ws_same_room"])
-hdataset, new_nxdatset = dataset_generator.nxdataset_to_training_hdata(filtered_nxdataset)
-# dataset_generator.reintroduce_predicted_edges(new_nxdatset["train"][0], [], "testing custom mp graph")
+dataset_generator = SquaredRoomDatasetGenerator(settings)
+# room_clustering_dataset = dataset_generator.get_ws2room_clustering_datalodaer()
+# filtered_nxdataset = dataset_generator.get_filtered_datset(["ws"],["ws_same_room"])
+# hdataset, new_nxdatset = dataset_generator.nxdataset_to_training_hdata(filtered_nxdataset)
+# # dataset_generator.reintroduce_predicted_edges(new_nxdatset["train"][0], [], "testing custom mp graph")
 
-gnn_wrapper = GNNWrapper(hdataset, settings)
+# gnn_wrapper = GNNWrapper(hdataset, settings)
 
-gnn_wrapper.define_GCN()
+# gnn_wrapper.define_GCN()
 
-gnn_wrapper.train(verbose= True)
+# gnn_wrapper.train(verbose= True)
 
-# gt_base_graphnx, unparented_base_graphnx, hdata_graph, node_label_mapping, ground_truth, gt_edges = dataset.get_ws2room_clustering_single_base_knn_graph(visualize=True)
-predicted_edges = gnn_wrapper.infer(hdataset, True)
-inference_base_graph = new_nxdatset["inference"][0]
-dataset_generator.reintroduce_predicted_edges(inference_base_graph, [], "Inference: ground truth")
-inference_base_graph.remove_all_edges()
-dataset_generator.reintroduce_predicted_edges(inference_base_graph, predicted_edges, "Inference: predictions")
+# # gt_base_graphnx, unparented_base_graphnx, hdata_graph, node_label_mapping, ground_truth, gt_edges = dataset.get_ws2room_clustering_single_base_knn_graph(visualize=True)
+# predicted_edges = gnn_wrapper.infer(hdataset, True)
+# inference_base_graph = new_nxdatset["inference"][0]
+# dataset_generator.reintroduce_predicted_edges(inference_base_graph, [], "Inference: ground truth")
+# inference_base_graph.remove_all_edges()
+# dataset_generator.reintroduce_predicted_edges(inference_base_graph, predicted_edges, "Inference: predictions")
 
 # mp_edges, label_edges = gnn_wrapper.get_message_sharing_edges(gt_base_graphnx)
 # dataset.reintroduce_predicted_edges(unparented_base_graphnx, mp_edges["train"], "train: mp_edges")
