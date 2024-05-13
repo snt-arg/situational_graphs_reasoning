c2a8ada8c42e5edde487679e0d3865f908a100c6
Hriday Bavle <hriday.bavle@gmail.com>
2024-05-13 15:35:13 +0200

    [Feat] Change s_graphs_msgs->situational_graphs_msgs

--- situational_graphs_reasoning/situational_graphs_reasoning_node.py
+++ situational_graphs_reasoning/situational_graphs_reasoning_node.py
@@ -38,11 +38,11 @@ from rclpy.parameter import ParameterType
 from ament_index_python.packages import get_package_share_directory
 from shapely.geometry import Polygon
 
-from s_graphs_msgs.msg import PlanesData as PlanesDataMsg
-from s_graphs_msgs.msg import RoomsData as RoomsDataMsg
-from s_graphs_msgs.msg import RoomData as RoomDataMsg
-from s_graphs_msgs.msg import WallsData as WallsDataMsg
-from s_graphs_msgs.msg import WallData as WallDataMsg
+from situational_graphs_msgs.msg import PlanesData as PlanesDataMsg
+from situational_graphs_msgs.msg import RoomsData as RoomsDataMsg
+from situational_graphs_msgs.msg import RoomData as RoomDataMsg
+from situational_graphs_msgs.msg import WallsData as WallsDataMsg
+from situational_graphs_msgs.msg import WallData as WallDataMsg
 
 from .GNNWrapper import GNNWrapper
 from situational_graphs_wrapper.GraphWrapper import GraphWrapper
